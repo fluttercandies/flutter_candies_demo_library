0b966cc23cb948b28a98d1392a737dedc245749b
zmtzawqlp <zmtzawqlp@live.com>
2020-01-29 21:21:20 +0800

    add flutter_candies_demo_library

---
+++ lib/flutter_candies_demo_library.dart
@@ -0,0 +1,11 @@
+library flutter_candies_demo_library;
+
+export 'package:flutter_candies_demo_library/src/data/mock_data.dart';
+export 'package:flutter_candies_demo_library/src/data/tu_chong_repository.dart';
+export 'package:flutter_candies_demo_library/src/data/tu_chong_source.dart';
+
+export 'package:flutter_candies_demo_library/src/utils/screen_util.dart';
+
+export 'package:loading_more_list_library/loading_more_list_library.dart';
+
+export 'package:http_client_helper/http_client_helper.dart';
